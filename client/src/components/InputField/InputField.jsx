1958cf55737a32e2b4429015fcd46acf221ac7a0
LethalPants <48921721+LethalPants@users.noreply.github.com>
2020-06-23 14:18:08 +0300

    Normalize files

--- client/src/components/InputField/InputField.jsx
+++ client/src/components/InputField/InputField.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './InputField.styles.css';
-const InputField = ({ label, ...props }) => {
-  return (
-    <div className="input-div">
-      <input {...props} className="form-input" />
-      {label ? <label className="form-label">{label}</label> : null}
-    </div>
-  );
-};
+
+const InputField = ({ label, ...props }) => (
+  <div className="input-div">
+    <input {...props} className="form-input" />
+    {label ? <label className="form-label">{label}</label> : null}
+  </div>
+);
 
 export default InputField;
--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import InputField from '../../components/InputField/InputField';
+import InputField from '../InputField/InputField';
 import SubmitButton from '../CustomButton/CustomButton';
 import './LoginForm.styles.css';
-const LoginForm = () => {
-  return (
-    <div className="login-form-container">
-      <div className="login-form">
-        <h1>Log into your account.</h1>
-        <form>
-          <InputField
-            name="email"
-            label="Email"
-            type="email"
-            required
-            placeholder=" "
-          />
-          <InputField
-            name="password"
-            label="Password"
-            type="password"
-            required
-            placeholder=" "
-          />
-          <SubmitButton>Login</SubmitButton>
-        </form>
-      </div>
+
+const LoginForm = () => (
+  <div className="login-form-container">
+    <div className="login-form">
+      <h1>Log into your account.</h1>
+      <form>
+        <InputField
+          name="email"
+          label="Email"
+          type="email"
+          required
+          placeholder=" "
+        />
+        <InputField
+          name="password"
+          label="Password"
+          type="password"
+          required
+          placeholder=" "
+        />
+        <SubmitButton>Login</SubmitButton>
+      </form>
     </div>
-  );
-};
+  </div>
+);
 
 export default LoginForm;
