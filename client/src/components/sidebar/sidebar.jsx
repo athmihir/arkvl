b6a056ee74f2d17f1e954ecec8e58296432b736e
LethalPants <48921721+LethalPants@users.noreply.github.com>
2020-07-15 13:53:08 +0300

    Fix sidebar issues

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import axios from 'axios';
 import { ReactComponent as BookIcon } from '../../assets/icons/menu_book-24px.svg';
 import { ReactComponent as TrendingIcon } from '../../assets/icons/trending_up-24px.svg';
-import { ReactComponent as ChevronIcon } from '../../assets/icons/ArrowPointer.svg';
 import { ReactComponent as UserAvatar } from '../../assets/user.svg';
 import { userLogout } from '../../redux/rootreducer';
 import './sidebar.styles.css';
@@ -18,18 +17,15 @@ const SideBar = ({ userName, logout, history }) => {
       history.push('/');
     });
   };
-  const [open, setOpen] = useState(false);
-  const toggleMenu = () => setOpen(!open);
   return (
     <>
-      <nav className={`navbar ${open ? 'open' : ''}`}>
+      <nav className="navbar">
         <ul className="navbar-nav">
           <li className="logo">
             <h3 className="logo-link">
               <Link to="/" className="link-text logo-text">
-                Reco
+                Arkvl
               </Link>
-              <ChevronIcon onClick={toggleMenu} />
             </h3>
           </li>
 
@@ -53,27 +49,13 @@ const SideBar = ({ userName, logout, history }) => {
 
           <li className="nav-item">
             <span className="nav-link user-avatar">
-              <UserAvatar onClick={toggleMenu} />
-              <div className="link-text">
-                <NavLink to="/user-profile" className="user-username">
-                  {userName}
-                </NavLink>
-                <button className="user-logout" onClick={handleLogout}>
-                  logout
-                </button>
-              </div>
+              <NavLink to="/user-profile" className="user-username">
+                <UserAvatar />
+              </NavLink>
             </span>
           </li>
         </ul>
       </nav>
-      <div className="hand-held-menu">
-        <Link to="/user-profile" className="user-username">
-          {userName}
-        </Link>
-        <button className="user-logout" onClick={handleLogout}>
-          logout
-        </button>
-      </div>
     </>
   );
 };
